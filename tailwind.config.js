d70aaf0425197417ac8e29e7b76f9488ebea196e
Stiven Giraldo <giraldostiven150@gmail.com>
2026-03-23 18:25:29 -0500

    Se organiza fonts y el copy de la sección de home

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          cormorant: ['"Cormorant Garamond"', 'serif'],
+          montserrat: ['Montserrat', 'sans-serif'],
+        },
+      },
+    },
+  }
